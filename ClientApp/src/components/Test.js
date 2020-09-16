d8453499dc98fcecf1e8fea92a8e17705ccdd7af
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-15 21:15:31 -0700

    Add files via upload

--- ClientApp/src/components/Test.js
+++ ClientApp/src/components/Test.js
@@ -27,20 +27,30 @@ const Library = ({ chars }) => {
 
 export class Test extends Component {
     static displayName = Test.name;
-
+    state = { available: false }
+/* state = { available: true } takes place of constructor
     constructor(props) {
         super(props)
         this.state = {
             available: true
         }
+        this.toggleAvailable = this.toggleAvailable.bind(this)
+    }
+    */
+
+    toggleAvailable = () => {
+        this.setState(prevState => ({
+            available: !prevState.available
+        }))
     }
 
     render() {
     return (
       <div>
             <h1>Characters are {this.state.available ? 'available' : 'all dead'}</h1>
+            <button onClick={this.toggleAvailable}>Kill/rez all</button>
             <Library chars={charList} />
-      </div>
+        </div>
     );
   }
 }
